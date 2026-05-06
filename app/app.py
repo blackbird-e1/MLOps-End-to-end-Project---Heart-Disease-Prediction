e26d597e24514ae00a34d9f516b8f030327adcdd
subhrajit95 <subhrajitshome95@gmail.com>
2026-05-06 13:04:19 +0530

    Added Prometheus and Grafana monitoring

--- app/app.py
+++ app/app.py
@@ -1,13 +1,47 @@
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+# from typing import List
+# import joblib
+# import numpy as np
+
+# app = FastAPI()
+
+# # Load model
+# model = joblib.load("models/model.pkl")
+
+# class InputData(BaseModel):
+#     data: List[float]
+
+
+# @app.get("/")
+# def home():
+#     return {"message": "Heart Disease Prediction API is running"}
+
+
+# @app.post("/predict")
+# def predict(input: InputData):
+#     arr = np.array(input.data).reshape(1, -1)
+#     prediction = model.predict(arr)
+
+#     return {
+#         "prediction": int(prediction[0]),
+#         "result": "Disease" if prediction[0] == 1 else "Healthy"
+#     }
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
 import joblib
 import numpy as np
 
+# NEW
+from prometheus_fastapi_instrumentator import Instrumentator
+
 app = FastAPI()
 
 # Load model
 model = joblib.load("models/model.pkl")
+
 
 class InputData(BaseModel):
     data: List[float]
@@ -20,10 +54,16 @@ def home():
 
 @app.post("/predict")
 def predict(input: InputData):
+
     arr = np.array(input.data).reshape(1, -1)
+
     prediction = model.predict(arr)
 
     return {
         "prediction": int(prediction[0]),
         "result": "Disease" if prediction[0] == 1 else "Healthy"
     }
+
+
+# NEW
+Instrumentator().instrument(app).expose(app)
